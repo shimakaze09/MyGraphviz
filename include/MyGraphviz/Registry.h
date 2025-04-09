23bb417fc2194a439ba85a657396dc474db10204
John <yangtianzhuo970425@gmail.com>
2025-04-09 23:53:28 +1200

    Update project version to 0.2.0 and MyCMake version to 0.6.4; refactor size_t to std::size_t in various classes for consistency

--- include/MyGraphviz/Registry.h
+++ include/MyGraphviz/Registry.h
@@ -15,34 +15,36 @@ class Registry {
 
   const std::vector<std::string>& GetNodes() const noexcept { return nodes; }
 
-  const std::vector<std::pair<size_t, size_t>>& GetEdges() const noexcept {
+  const std::vector<std::pair<std::size_t, std::size_t>>& GetEdges()
+      const noexcept {
     return edges;
   }
 
   using ElemAttrMap =
-      std::map<size_t, std::map<std::string, std::string, std::less<>>>;
+      std::map<std::size_t, std::map<std::string, std::string, std::less<>>>;
 
   const ElemAttrMap& GetNodeAttrs() const noexcept { return nodeAttrs; }
 
   const ElemAttrMap& GetEdgeAttrs() const noexcept { return edgeAttrs; }
 
   bool IsRegisteredNode(std::string_view ID) const;
-  bool IsRegisteredEdge(size_t lhs, size_t rhs) const;
+  bool IsRegisteredEdge(std::size_t lhs, std::size_t rhs) const;
   bool IsRegisteredEdge(std::string_view lhsID, std::string_view rhsID) const;
 
-  size_t GetNodeIndex(std::string_view ID) const;
-  size_t GetEdgeIndex(std::string_view lhsID, std::string_view rhsID) const;
+  std::size_t GetNodeIndex(std::string_view ID) const;
+  std::size_t GetEdgeIndex(std::string_view lhsID,
+                           std::string_view rhsID) const;
 
-  size_t RegisterNode(std::string ID);
-  size_t RegisterEdge(size_t lhs, size_t rhs);
+  std::size_t RegisterNode(std::string ID);
+  std::size_t RegisterEdge(std::size_t lhs, std::size_t rhs);
 
-  Registry& RegisterNodeAttr(size_t nodeIndex, std::string key,
+  Registry& RegisterNodeAttr(std::size_t nodeIndex, std::string key,
                              std::string value);
-  Registry& RegisterEdgeAttr(size_t edgeIndex, std::string key,
+  Registry& RegisterEdgeAttr(std::size_t edgeIndex, std::string key,
                              std::string value);
 
-  Registry& DeregisterNodeAttr(size_t nodeIndex, std::string_view key);
-  Registry& DeregisterEdgeAttr(size_t edgeIndex, std::string_view key);
+  Registry& DeregisterNodeAttr(std::size_t nodeIndex, std::string_view key);
+  Registry& DeregisterEdgeAttr(std::size_t edgeIndex, std::string_view key);
 
  private:
   Registry(const Registry&) = delete;
@@ -51,11 +53,12 @@ class Registry {
   Registry& operator=(Registry&&) = delete;
 
   std::vector<std::string> nodes;
-  std::map<std::string, size_t, std::less<>> id2idx;
+  std::map<std::string, std::size_t, std::less<>> id2idx;
   ElemAttrMap nodeAttrs;
 
-  std::vector<std::pair<size_t, size_t>> edges;
-  std::map<size_t, std::map<size_t, size_t>, std::less<>> node2edge;
+  std::vector<std::pair<std::size_t, std::size_t>> edges;
+  std::map<std::size_t, std::map<std::size_t, std::size_t>, std::less<>>
+      node2edge;
   ElemAttrMap edgeAttrs;
 };
 }  // namespace My::MyGraphviz
--- src/core/Subgraph.cpp
+++ src/core/Subgraph.cpp
@@ -58,35 +58,36 @@ Subgraph& Subgraph::GenSubgraph(std::string ID) {
   return *subgraphs.back();
 }
 
-bool Subgraph::HaveNode(size_t nodeIndex) const {
+bool Subgraph::HaveNode(std::size_t nodeIndex) const {
   return nodeIndices.find(nodeIndex) != nodeIndices.end();
 }
 
-bool Subgraph::HaveEdge(size_t edgeIndex) const {
+bool Subgraph::HaveEdge(std::size_t edgeIndex) const {
   return edgeIndices.find(edgeIndex) != edgeIndices.end();
 }
 
-Subgraph& Subgraph::AddNode(size_t nodeIndex) {
+Subgraph& Subgraph::AddNode(std::size_t nodeIndex) {
   nodeIndices.insert(nodeIndex);
   return *this;
 }
 
-Subgraph& Subgraph::AddEdge(size_t edgeIndex) {
+Subgraph& Subgraph::AddEdge(std::size_t edgeIndex) {
   edgeIndices.insert(edgeIndex);
   return *this;
 }
 
-Subgraph& Subgraph::EraseNode(size_t nodeIndex) {
+Subgraph& Subgraph::EraseNode(std::size_t nodeIndex) {
   nodeIndices.erase(nodeIndex);
   return *this;
 }
 
-Subgraph& Subgraph::EraseEdge(size_t edgeIndex) {
+Subgraph& Subgraph::EraseEdge(std::size_t edgeIndex) {
   edgeIndices.erase(edgeIndex);
   return *this;
 }
 
-std::string Subgraph::Dump(bool isSub, bool isDigraph, size_t indent) const {
+std::string Subgraph::Dump(bool isSub, bool isDigraph,
+                           std::size_t indent) const {
   if (!isSub && subgraphs.empty() && nodeIndices.empty() && edgeIndices.empty())
     return "";
 
@@ -94,12 +95,12 @@ std::string Subgraph::Dump(bool isSub, bool isDigraph, size_t indent) const {
 
   std::string eop = isDigraph ? "->" : "--";
 
-  auto quote = [](std::string_view id) {
+  auto qoute = [](std::string_view id) {
     return "\"" + std::string(id) + "\"";
   };
 
   auto print_indent = [&]() -> std::stringstream& {
-    for (size_t i = 0; i < indent; i++)
+    for (std::size_t i = 0; i < indent; i++)
       ss << "  ";
     return ss;
   };
@@ -109,7 +110,7 @@ std::string Subgraph::Dump(bool isSub, bool isDigraph, size_t indent) const {
   if (isSub)
     ss << "sub";
 
-  ss << "graph " << quote(id) << " {" << std::endl;
+  ss << "graph " << qoute(id) << " {" << std::endl;
 
   indent++;
 
@@ -143,19 +144,19 @@ std::string Subgraph::Dump(bool isSub, bool isDigraph, size_t indent) const {
   const auto& nodeAttrs = registry->GetNodeAttrs();
   const auto& edgeAttrs = registry->GetEdgeAttrs();
 
-  for (size_t nodeIndex : nodeIndices) {
+  for (std::size_t nodeIndex : nodeIndices) {
     const auto& nodeID = nodeIDs[nodeIndex];
     auto target = nodeAttrs.find(nodeIndex);
     if (target == nodeAttrs.end())
-      print_indent() << quote(nodeID) << std::endl;
+      print_indent() << qoute(nodeID) << std::endl;
     else
-      dumpAttrs(quote(nodeID), target->second);
+      dumpAttrs(qoute(nodeID), target->second);
   }
 
-  for (size_t edgeIndex : edgeIndices) {
+  for (std::size_t edgeIndex : edgeIndices) {
     const auto& [lhs, rhs] = edgeIDs[edgeIndex];
     std::string head =
-        quote(nodeIDs[lhs]) + " " + eop + " " + quote(nodeIDs[rhs]);
+        qoute(nodeIDs[lhs]) + " " + eop + " " + qoute(nodeIDs[rhs]);
     auto target = edgeAttrs.find(edgeIndex);
     if (target == edgeAttrs.end())
       print_indent() << head << std::endl;
